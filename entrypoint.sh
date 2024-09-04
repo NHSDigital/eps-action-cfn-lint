815f016ace335381410847c8f00734949ad6da6f
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-09-04 13:44:44 +0100

    show version

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,11 @@
 #!/bin/sh -l
 
+cfn-lint -v
 echo "Running cfn lint against cloudformation directory"
-cfn-lint -I "/github/workspace/cloudformation/**/*.y*ml" 
-
-# cfn-lint -I "/github/workspace/cloudformation/**/*.y*ml" 2>&1 | awk '/Run scan/ { print } /^[EW][0-9]/ { print; getline; print }'
+cfn-lint -I "/github/workspace/cloudformation/**/*.y*ml" 2>&1 | awk '/Run scan/ { print } /^[EW][0-9]/ { print; getline; print }'
 
 echo "Running cfn-lint against SAMtemplates directory"
-cfn-lint -I "/github/workspace/SAMtemplates/**/*.y*ml"
-
-#cfn-lint -I "/github/workspace/SAMtemplates/**/*.y*ml" 2>&1 | awk '/Run scan/ { print } /^[EW][0-9]/ { print; getline; print }'
+cfn-lint -I "/github/workspace/SAMtemplates/**/*.y*ml" 2>&1 | awk '/Run scan/ { print } /^[EW][0-9]/ { print; getline; print }'
 
 
 
