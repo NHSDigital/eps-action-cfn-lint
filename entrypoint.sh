9199fff19e8c719c5ec8c8b0a303f2763d7e12b1
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-09-04 11:46:18 +0100

    debug in docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/sh -l
 
+echo "Running ls /github/workspace"
+
+ls /github/workspace
+
+echo "Running cfn lint against cloudformation directory"
 
 cfn-lint -I "/github/workspace/cloudformation/**/*.y*ml" 2>&1 | awk '/Run scan/ { print } /^[EW][0-9]/ { print; getline; print }'
+
+echo "Running cfn-lint against SAMtemplates directory"
+
 cfn-lint -I "/github/workspace/SAMtemplates/**/*.y*ml" 2>&1 | awk '/Run scan/ { print } /^[EW][0-9]/ { print; getline; print }'
 
 
