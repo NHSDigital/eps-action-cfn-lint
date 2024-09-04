f34c4d7e66fcf3abfb3062e90d56ec08257b5eb3
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-09-04 13:59:20 +0100

    correct output

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,8 +12,8 @@ echo
 echo "Running cfn-lint against SAMtemplates directory"
 cfn-lint -I "/github/workspace/SAMtemplates/**/*.y*ml" > samtemplates_scan.txt 2>&1 
 
-awk '/Run scan/ { print } /^[EW][0-9]/ { print; getline; print }'  samtemplates_scan.txt 
-
+awk '/Run scan/ { print } /^[EW][0-9]/ { print; getline; print }'  samtemplates_scan.txt
+echo
 
 if grep -q '^[EW][0-9]' 'cloudformation_scan.txt'
 then
@@ -28,5 +28,4 @@ then
     exit 1
 fi
 
-echo
 echo "No errors found"
