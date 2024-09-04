67b1a520c416ef0106713d35970c745006dfa328
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-09-04 14:22:04 +0100

    better comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,16 +11,18 @@ echo
 echo "Running cfn lint against cloudformation directory"
 cfn-lint -i E0000 -I "/github/workspace/cloudformation/**/*.y*ml" > cloudformation_scan.txt  2>&1 
 
+# display what files we scanned and any errors or warnings with surrounding lines for context
 awk '/Run scan/ { print } /^[EW][0-9]/ { print; getline; print }' cloudformation_scan.txt
 echo
 
 echo "Running cfn-lint against SAMtemplates directory"
 cfn-lint -I "/github/workspace/SAMtemplates/**/*.y*ml" > samtemplates_scan.txt 2>&1 
 
+# display what files we scanned and any errors or warnings with surrounding lines for context
 awk '/Run scan/ { print } /^[EW][0-9]/ { print; getline; print }'  samtemplates_scan.txt
 echo
 
-# exit with if there were errors
+# exit with error if there were errors from cfn-lint
 if grep -q '^[EW][0-9]' 'cloudformation_scan.txt'
 then
     echo "Errors were found"
